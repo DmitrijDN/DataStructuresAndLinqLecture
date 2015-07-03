24598a08e3534016c37966f143af6eb1676a28ff
Beseda Dmitriy <BesedaDG@gmail.com>
2015-07-03 06:43:28 +0300

    Added examples for boxing and unboxing execution time difference

---
+++ DataStructuresAndLINQ/BoxingUnboxingExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            for (var i = 0; i < 10000000; i++)
+            {
+                WithBoxing();
+            }
+            var end = DateTime.Now;
+            var duration = end - start;
+            Console.WriteLine("Time With boxing: {0}", duration.Milliseconds);
+
+            start = DateTime.Now;
+            for (var i = 0; i < 10000000; i++)
+            {
+                WithoutBoxing();
+            }
+            end = DateTime.Now;
+            duration = end - start;
+            Console.WriteLine("Time without unboxing: {0}", duration.Milliseconds);
+
+            Console.ReadLine();
+
+        }
+
+        public static void WithBoxing()
+        {
+            var a = 1;
+            object some = a;
+        }
+
+        public static void WithoutBoxing()
+        {
+            var a = 5;
+            object some = a;
+        }
+    }
+}
---
+++ DataStructuresAndLINQ/GenericVsNonGeneric/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericVsNonGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var genericList = new List<int>();
+            var nonGenericList = new ArrayList();
+
+            for (var i = 0; i < 10000000; i++)
+            {
+                genericList.Add(1);
+                nonGenericList.Add(1);
+            }
+
+            var start = DateTime.Now;
+            double result = genericList.Sum();
+            var end = DateTime.Now;
+            var duration = end - start;
+            Console.WriteLine("Generic result: {0}\n generic time:{1}", result, duration.Milliseconds);
+
+            result = 0;
+            start = DateTime.Now;
+            result = nonGenericList.Cast<int>().Sum();
+            end = DateTime.Now;
+            duration = end - start;
+            Console.WriteLine("Generic result: {0}\n nongeneric time:{1}", result, duration.Milliseconds);
+
+            Console.ReadLine();
+        }
+    }
+}
